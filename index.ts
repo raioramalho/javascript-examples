6eee83fa4837b797fda9c9f30c5882d67a895586
Alan Ramalho <ramalho.sit@gmail.com>
2023-05-14 18:48:33 -0300

    zod

--- index.ts
+++ index.ts
@@ -1,2 +1,21 @@
-console.clear()
+import { error } from 'console';
+import { type } from 'os';
+import { z } from 'zod';
 
+const PersonSchema = z.object({
+  id: z.number().min(1),
+  name: z.string().min(3),
+  email: z.string().email()
+})
+
+type Person = z.infer<typeof PersonSchema>;
+
+let person = {
+  id: 2,
+  name: 'Alan',
+  email: 'ramalho@gmail.com'
+}
+
+const result = PersonSchema.safeParse(person);
+
+console.log(result)
