a09b2b48ce1cdd95dc8bfeb9adccbfbebd8e69ad
Alan Ramalho <ramalho.sit@gmail.com>
2023-05-19 16:17:46 -0300

    format

--- typesolid/items/item.ts
+++ typesolid/items/item.ts
@@ -1,7 +1,7 @@
 export default class Item {
-  constructor(public category: string, public description: string, public price: number) {
-    this.category = category;
-    this.description = description;
-    this.price = price;
-  }
+  constructor(
+    public category: string,
+    public description: string,
+    public price: number
+  ) { }
 }
