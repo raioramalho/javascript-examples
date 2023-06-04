9a3c3a8c85404f1f4fe77f57c95dfd0f45092526
Alan Ramalho <ramalho.sit@gmail.com>
2023-06-04 19:55:58 -0300

    aff

--- index.ts
+++ index.ts
@@ -1,14 +1,13 @@
 import fetch from 'node-fetch';
 import * as fs from 'fs';
-import { setTimeout } from 'timers/promises';
 
 class ImageService {
   async download(url: string, path: string) {
     var sucess = false;
     fetch(url)
-      .then(async (response: any) => {
+      .then((response: any) => {
         const dest = fs.createWriteStream(path);
-        await response.body.pipe(dest);
+        response.body.pipe(dest);
 
         dest.on('finish', () => {
           console.log('Download concluído!');
@@ -23,7 +22,7 @@ class ImageService {
         });
 
       });
-    return sucess;
+    return await sucess;
   }
 }
 
@@ -33,4 +32,6 @@ const imageService = new ImageService();
 let imgUrl = "https://images.unsplash.com/photo-1662010021854-e67c538ea7a9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2427&q=80";
 
 
-await imageService.download(imgUrl, './public/img.jpg');
+imageService.download(imgUrl, './public/img.jpg');
+
+
