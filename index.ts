0091eee9c70b351842e49143735d71c4ed241ccb
Alan Ramalho <ramalho.sit@gmail.com>
2023-06-04 19:51:38 -0300

    fix

--- index.ts
+++ index.ts
@@ -33,6 +33,4 @@ const imageService = new ImageService();
 let imgUrl = "https://images.unsplash.com/photo-1662010021854-e67c538ea7a9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2427&q=80";
 
 
-console.log(await imageService.download(imgUrl, './public/img.jpg'));
-
-
+await imageService.download(imgUrl, './public/img.jpg');
