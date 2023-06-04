d63b040de0260a7a4fb07446fa2139db82eb1c1a
Alan Ramalho <ramalho.sit@gmail.com>
2023-06-04 19:49:38 -0300

    ok!

--- index.ts
+++ index.ts
@@ -1,7 +1,10 @@
 import fetch from 'node-fetch';
 import * as fs from 'fs';
+import { setTimeout } from 'timers/promises';
+
 class ImageService {
   async download(url: string, path: string) {
+    var sucess = false;
     fetch(url)
       .then(async (response: any) => {
         const dest = fs.createWriteStream(path);
@@ -9,30 +12,27 @@ class ImageService {
 
         dest.on('finish', () => {
           console.log('Download concluído!');
+          sucess = true;
           return true;
         });
 
         dest.on('error', (err) => {
           console.error(`Ocorreu um erro durante o download: ${err.message}`);
-          return false;
+          // return false;
+          throw new Error(err.message);
         });
-      })
-      .catch(error => {
-        console.error(`Ocorreu um erro durante a solicitação: ${error.message}`);
-        return false;
+
       });
+    return sucess;
   }
 }
 
 const imageService = new ImageService();
 
+
 let imgUrl = "https://images.unsplash.com/photo-1662010021854-e67c538ea7a9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2427&q=80";
 
 
-const test = imageService.download(imgUrl, './public/img.jpg');
+console.log(await imageService.download(imgUrl, './public/img.jpg'));
 
-if (!test) {
-  console.log('not ok');
-} else {
-  console.log('ok!')
-}
+
