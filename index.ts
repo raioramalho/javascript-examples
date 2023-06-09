4ee6d886044327f27bb768c7b259bcb27311a98f
Alan Ramalho <ramalho.sit@gmail.com>
2023-06-09 11:40:12 -0300

    test

--- index.ts
+++ index.ts
@@ -1,37 +1,45 @@
-import fetch from 'node-fetch';
+import EventEmitter from "events"
 import * as fs from 'fs';
+import axios from 'axios';
 
+const ev = new EventEmitter()
 class ImageService {
   async download(url: string, path: string) {
-    var sucess = false;
-    fetch(url)
-      .then((response: any) => {
-        const dest = fs.createWriteStream(path);
-        response.body.pipe(dest);
+    const response = await axios.get(url, { responseType: 'stream' });
+    response.data.pipe(fs.createWriteStream(path));
 
-        dest.on('finish', () => {
-          console.log('Download concluído!');
-          sucess = true;
-          return true;
-        });
-
-        dest.on('error', (err) => {
-          console.error(`Ocorreu um erro durante o download: ${err.message}`);
-          // return false;
-          throw new Error(err.message);
-        });
-
+    return new Promise<void>((resolve, reject) => {
+      response.data.on('end', () => {
+        resolve();
+        // console.log('test')
+        ev.emit('saySomething')
       });
-    return await sucess;
+
+      response.data.on('error', (err) => {
+        reject(err);
+      });
+    });
   }
 }
 
 const imageService = new ImageService();
 
 
-let imgUrl = "https://images.unsplash.com/photo-1662010021854-e67c538ea7a9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2427&q=80";
+let imgUrl = "http://dispan.nuvemdatacom.com.br:9740/mge/Produto@IMAGEM@CODPROD=23.dbimage";
 
 
-imageService.download(imgUrl, './public/img.jpg');
+let lista = 4;
+
+for (let index = 0; index < lista; index++) {
+  imageService.download(imgUrl, './public/img2.jpg')
+}
+
+
+ev.on('saySomething', () => {
+  console.log("Baixou...")
+})
+
+
+
 
 
