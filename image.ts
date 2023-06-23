be55a487529cc712d35b0083b55c7b3d9e3fa160
Alan Ramalho <ramalho.sit@gmail.com>
2023-06-23 20:11:25 -0300

    up

--- image.ts
+++ image.ts
@@ -1,40 +1,58 @@
-import EventEmitter from "events"
 import * as fs from 'fs';
 import axios from 'axios';
 
-const ev = new EventEmitter()
 class ImageService {
-  async download(url: string, path: string) {
-    const response = await axios.get(url, { responseType: 'stream' });
-    response.data.pipe(fs.createWriteStream(path));
+  async baixarImagem(url: string, path: string, name: any): Promise<void> {
+    try {
 
-    return new Promise<void>((resolve, reject) => {
-      response.data.on('end', () => {
-        resolve();
-        // console.log('test')
-        ev.emit('saySomething')
-      });
+      await fs.rm(`${path}/${name}`, (resolve) => {
 
-      response.data.on('error', (err) => {
-        reject(err);
+      })
+      const response = await axios.get(url, { responseType: 'stream' });
+      response.data.pipe(fs.createWriteStream(`${path}/${name}`));
+
+      await new Promise<void>((resolve, reject) => {
+        response.data.on('end', () => {
+          resolve();
+          console.log(`Baixou: ${name}`);
+        });
+
+        response.data.on('error', () => {
+          // console.log(`Não Baixou: ${id}`);
+          resolve();
+        });
       });
-    });
+    } catch (error) {
+      console.log(`Não Baixou: ${name}`);
+    }
   }
+
+  buscaImagem = async (caminhoArquivo: string): Promise<boolean> => {
+    try {
+      await fs.accessSync(caminhoArquivo);
+      return true;
+    } catch (error) {
+      return false;
+    }
+  };
 }
+
 
 const imageService = new ImageService();
 
 
-let imgUrl = "http://dispan.nuvemdatacom.com.br:9740/mge/Produto@IMAGEM@CODPROD=23.dbimage";
+const lista = [
+  "https://apexensino.com.br/wp-content/uploads/2017/10/fronback.png",
+  "https://blog.back4app.com/wp-content/uploads/2021/06/backend-vs-frontend-2.png",
+  "http://apexensino.com.br/wp-content/uploads/2017/10/fronback.png",
+  "https://www.gobacklog.com/wp-content/uploads/2020/05/Desenvolvimento-Back-End-Um-Guia-para-Empreendedores-Afinal-o-que-%C3%A9-Back-End-1-1024x536.jpg"
+];
 
+var novaLista = []
 
-let lista = 4;
-
-for (let index = 0; index < lista; index++) {
-  imageService.download(imgUrl, './public/img2.jpg')
+for (let index = 0; index < lista.length; index++) {
+  await imageService.baixarImagem(lista[index], `./public/img${index}.jpg`, index);
+  novaLista.push({src: `http://public/img${index}.jpg`});
 }
 
-
-ev.on('saySomething', () => {
-  console.log("Baixou...")
-})
+console.log(novaLista);
