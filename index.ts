066e5456dcf97d670cfc5752db122b0ba300a1cc
Alan Ramalho <ramalho.sit@gmail.com>
2023-07-21 21:32:46 -0300

    pedidos

---
+++ index.ts
@@ -0,0 +1,16 @@
+const lista = [
+  {
+    id: 0,
+    name: 'Viviane'
+  },
+  {
+    id: 1,
+    name: 'Alan'
+  }
+]
+
+
+const buscaPosicao = lista.findIndex((index) => index.CODTABPREC=== );
+
+
+console.log(buscaPosicao);
