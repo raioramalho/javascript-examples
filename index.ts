f484a137e352434ee5c834a4128bb2523a527dbc
Alan Ramalho <ramalho.sit@gmail.com>
2023-08-14 19:17:39 -0300

    woo comer

---
+++ index.ts
@@ -0,0 +1,12 @@
+import { fetchProducts } from './woocommerce';
+
+async function main() {
+  try {
+    const products = await fetchProducts();
+    console.log('Products:', products);
+  } catch (error) {
+    console.error('Main error:', error);
+  }
+}
+
+main();
