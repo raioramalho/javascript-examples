0949a248db76baa60e5da98e81eefd78eb5fa520
Alan Ramalho <ramalho.sit@gmail.com>
2023-08-14 22:01:10 -0300

    estudando typescript.

--- index.ts
+++ index.ts
@@ -1,6 +1,36 @@
-
-
 console.log('Starting...');
 
 
-let var1: string = 'hello world';
+export class PersonDto {
+  name: string;
+  lastName: string;
+  age: number;
+}
+
+
+type FindPerson = PersonDto[keyof PersonDto]
+
+const lista: PersonDto[] = [
+  { name: 'Alan', lastName: 'Ramalho', age: 26 },
+  { name: 'John', lastName: 'Doe', age: 25 },
+];
+
+
+function buscaPessoa(param: FindPerson): PersonDto | boolean {
+  const findByName = lista.filter((pessoa) => pessoa.name === param)[0];
+  const findByLastName = lista.filter((pessoa) => pessoa.lastName === param)[0];
+  const findByAge = lista.filter((pessoa) => pessoa.age === param)[0];
+  if (findByName) {
+    return findByName;
+  }
+  if (findByLastName) {
+    return findByLastName;
+  }
+  if (findByAge) {
+    return findByAge;
+  }
+
+  return false;
+}
+
+console.log(buscaPessoa('John'));
