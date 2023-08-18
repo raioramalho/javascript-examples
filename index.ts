e419379047ca008d19eb1790331def6dd9ebaab3
Alan Ramalho <ramalho.sit@gmail.com>
2023-08-18 02:14:09 -0300

    estudando compressao de json.

--- index.ts
+++ index.ts
@@ -1,14 +1,38 @@
-console.log('Starting...');
+import * as zlib from 'zlib';
+import * as fs from 'fs';
 
-import { fetchProducts } from './woocommerce';
 
-async function main() {
-  try {
-    const products = await fetchProducts();
-    console.log('Products:', products);
-  } catch (error) {
-    console.error('Main error:', error);
+console.log(`Starting..`)
+
+
+try {
+  const filePath = './produtos.json'; // Substitua pelo caminho correto
+  const rawData = fs.readFileSync(filePath, 'utf-8');
+  const jsonData = JSON.parse(rawData);
+
+  let qtdProdutos = 0;
+  let produtos: any[] = [];
+
+  for (let index = 0; index < jsonData.slice(0, 500).length; index++) {
+    console.log(`contando: ${jsonData[index].PRODUTO} - produto de numero ${index}`);
+    produtos.push(jsonData[index]);
+    qtdProdutos += index;
   }
-}
 
-main();
+  // console.log(produtos);
+  console.log(`Produtos: ${produtos.length}`);
+
+
+  zlib.gzip(JSON.stringify(produtos), (error, compressedData) => {
+    if (error) {
+      console.error('Erro ao compactar o JSON:', error);
+    } else {
+      // fs.writeFileSync('produtos_comprimidos.json.gz', compressedData);
+      console.log(`JSON comprimido e salvo com sucesso: ${compressedData.byteLength}`);
+    }
+  });
+
+
+} catch (error) {
+  console.error('Erro ao ler o arquivo JSON:', error);
+}
